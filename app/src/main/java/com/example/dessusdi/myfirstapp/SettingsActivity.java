73bb1deb50de4a5a92781014b75eb7c3be7aea95
Dimitri Dessus <iphoneretro@icloud.com>
2017-02-13 10:53:34 +0100

    [Add] Icon to customize header in settings activity

--- app/src/main/java/com/example/dessusdi/myfirstapp/SettingsActivity.java
+++ app/src/main/java/com/example/dessusdi/myfirstapp/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.dessusdi.myfirstapp;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.util.Log;
+import android.widget.ImageView;
 
 import java.util.List;
 
@@ -29,6 +31,7 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_customization);
+
         }
     }
 }
