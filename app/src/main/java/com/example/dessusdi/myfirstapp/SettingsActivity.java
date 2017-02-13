6cdda6ad98ba7246f79b41f96ebe53be9a98d0d5
Dimitri Dessus <iphoneretro@icloud.com>
2017-02-13 11:50:25 +0100

    [Update] Replace fragment to keep only one

--- app/src/main/java/com/example/dessusdi/myfirstapp/SettingsActivity.java
+++ app/src/main/java/com/example/dessusdi/myfirstapp/SettingsActivity.java
@@ -3,10 +3,6 @@ package com.example.dessusdi.myfirstapp;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.util.Log;
-import android.widget.ImageView;
-
-import java.util.List;
 
 /**
  * Created by dessusdi on 13/02/2017.
@@ -15,23 +11,23 @@ import java.util.List;
 
 public class SettingsActivity extends PreferenceActivity {
 
-    @Override
-    protected boolean isValidFragment(String fragmentName) {
-        return CustomizationFragment.class.getName().equals(fragmentName);
-    }
+    public SettingsActivity() {}
 
     @Override
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.settings, target);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Replace headers to global settings
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new GlobalFragment()).commit();
     }
 
-    public static class CustomizationFragment extends PreferenceFragment {
+    public static class GlobalFragment extends PreferenceFragment {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.settings_customization);
-
+            addPreferencesFromResource(R.xml.settings_global);
         }
     }
 }
