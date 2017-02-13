4eee94e8195481a3b3b98d63a1f945098a1df707
Dimitri Dessus <iphoneretro@icloud.com>
2017-02-13 10:31:16 +0100

    [Fix] Now using PreferenceFragment (fix deprecated method in Settings Activity)

--- app/src/main/java/com/example/dessusdi/myfirstapp/SettingsActivity.java
+++ app/src/main/java/com/example/dessusdi/myfirstapp/SettingsActivity.java
@@ -2,17 +2,33 @@ package com.example.dessusdi.myfirstapp;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import java.util.List;
 
 /**
  * Created by dessusdi on 13/02/2017.
  * DESSUS Dimitri
  */
+
 public class SettingsActivity extends PreferenceActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected boolean isValidFragment(String fragmentName) {
+        return CustomizationFragment.class.getName().equals(fragmentName);
+    }
 
-        addPreferencesFromResource(R.xml.settings);
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.settings, target);
+    }
 
+    public static class CustomizationFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_customization);
+        }
     }
 }
