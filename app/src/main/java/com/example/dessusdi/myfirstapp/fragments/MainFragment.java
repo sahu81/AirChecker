415ef0a9856085790c9e2c6efec7dde7b7bc10e4
KimNamhoon <namhoon0327@gmail.com>
2019-04-09 23:34:59 +0900

    1.날씨정보 수신 테스트 기능 구현중1
    1.날씨정보 수신 테스트 기능 구현중1

--- app/src/main/java/com/example/dessusdi/myfirstapp/fragments/MainFragment.java
+++ app/src/main/java/com/example/dessusdi/myfirstapp/fragments/MainFragment.java
@@ -1,6 +1,7 @@
 package com.example.dessusdi.myfirstapp.fragments;
 
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
@@ -9,7 +10,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
